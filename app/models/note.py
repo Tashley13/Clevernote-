471382e3714af3701279c4550542df5de4553afb
Taylor <taylorashley1304@gmail.com>
2024-07-30 19:04:39 -0400

    created the note model with relationships defined between user, notebook, and tag

---
+++ app/models/note.py
@@ -0,0 +1,31 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class Note(db.Model):
+    __tablename__= 'notes'
+
+    if environment == 'production':
+        __table_args__ = {'schema' : SCHEMA }
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    content = db.Column(db.String)
+    notebookId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('notebooks.id')), nullable=False)
+    userId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    created_at = db.Column(db.datetime)
+    updated_at = db.Column(db.datetime)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title' : self.title,
+            'content' : self.content,
+            'notebookId' : self.notebookId,
+            'userId' : self.userId,
+            'created_at': self.created_at,
+            'updated_at' : self.updated_at
+        }
+
+user = db.relationship('User', back_populates='notes')
+notebook = db.relationship('Notebook', back_populates='notes')
+# tag = db.relationship('Tag', back_populates='notes')
