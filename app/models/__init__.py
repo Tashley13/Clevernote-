1261031775ed009fe90674b270e3d6752dcec2f6
Taylor <taylorashley1304@gmail.com>
2024-07-30 20:57:26 -0400

    created seeder file for notes

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,4 @@ from .user import User
 from .task import Task  # Add this line to import the Task model
 from .db import environment, SCHEMA
 from .notebooks import Notebook
+from .notes import Note
