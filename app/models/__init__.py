eaf80dfe1f773bb1441028dc9f6fa2d1d08276dd
harmony <harmony.rosenberg94@gmail.com>
2024-07-30 19:07:32 -0500

    import errors

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from .db import db
 from .user import User
+from .note_tag import Note_Tag
 from .db import environment, SCHEMA
---
+++ app/models/tags.py
@@ -0,0 +1,21 @@
+from .db import db, environment, SCHEMA
+from datetime import datetime
+
+class Tag(db.Model):
+	__tablename__ = 'tags'
+
+	if environment == "production":
+		__table_args__ = {'schema': SCHEMA}
+
+	id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+	tag_name = db.Column(db.String(255), nullable = False)
+	created_at = db.Column(db.DateTime, default = datetime.utcnow)
+	updated_at = db.Column(db.DateTime, default = datetime.utcnow)
+
+	def to_dict(self):
+		return {
+			'id': self.id,
+			'tag_name': self.tag_name,
+			'created_at': self.created_at,
+			'updated_at': self.updated_at
+		}
