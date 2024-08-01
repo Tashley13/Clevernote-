f4746cbba6edfa18b36d95a76a5600f587288ece
Taylor <taylorashley1304@gmail.com>
2024-08-01 19:14:31 -0400

    fixed up seeder names, simplified notes seeder

--- app/models/note.py
+++ app/models/note.py
@@ -14,8 +14,8 @@ class Note(db.Model):
     content = db.Column(db.String)
     notebookId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('notebooks.id')), nullable=False)
     userId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
-    # created_at = db.Column(db.datetime)
-    # updated_at = db.Column(db.datetime)
+    created_at = db.Column(db.datetime)
+    updated_at = db.Column(db.datetime)
 
     user = db.relationship('User', back_populates='notes')
     notebook = db.relationship('Notebook', back_populates='notes')
@@ -28,6 +28,6 @@ class Note(db.Model):
             'content' : self.content,
             'notebookId' : self.notebookId,
             'userId' : self.userId,
-            # 'created_at': self.created_at,
-            # 'updated_at' : self.updated_at
+            'created_at': self.created_at,
+            'updated_at' : self.updated_at
         }
