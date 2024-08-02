c93a95b797b9e4a9ac7c9791352c175a0b39f8d6
frank <imbruglia100@gmail.com>
2024-08-02 18:46:22 -0400

    nav close and open function

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,21 +1,40 @@
 import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Navigation.css";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 function Navigation() {
+  const [navOpen, setNavOpen] = useState(true)
+
+  const navViewChanger = (e) => {
+    let navMain = document.getElementById("nav-main")
+    let arrow = document.getElementById("viewer-icon")
+    setNavOpen(!navOpen)
+
+    if(navOpen){
+      navMain.style.width = '50px'
+      arrow.style.rotate = '360deg'
+    }else{
+      navMain.style.width = '250px'
+      arrow.style.rotate = '180deg'
+    }
+  }
   return (
-    <nav>
-      <ul id="nav-list">
-        <li className="nav-list-profile">
-          <ProfileButton />
-        </li>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-
-
-      </ul>
-    </nav>
+      <nav id="nav-main">
+        <ul id="nav-list">
+          <li className="nav-list-profile">
+            <ProfileButton />
+          </li>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+        </ul>
+      <button id="nav-close-btn" onClick={navViewChanger}>
+        <FontAwesomeIcon id="viewer-icon" icon={faArrowRight} />
+      </button>
+      </nav>
   );
 }
 
