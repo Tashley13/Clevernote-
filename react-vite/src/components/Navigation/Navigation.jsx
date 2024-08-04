06082fdf3cadee44a8954741c29263656d92d89e
frank <imbruglia100@gmail.com>
2024-08-04 19:25:25 -0400

    Revert "added styling for nav bar"
    This reverts commit d1967e10db8941520573dae1840e6c1b1aab61b8.

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -6,80 +6,67 @@ import { faArrowRight, faBook, faFileLines, faHouse, faListCheck, faTags } from 
 import { useState } from "react";
 
 function Navigation() {
-  const [navOpen, setNavOpen] = useState(true);
+  const [navOpen, setNavOpen] = useState(true)
 
   const navViewChanger = () => {
-    const navMain = document.getElementById("nav-main");
-    const arrow = document.getElementById("viewer-icon");
-    const innerText = document.querySelectorAll('.nav-inner-text');
-    setNavOpen(!navOpen);
+    const navMain = document.getElementById("nav-main")
+    const arrow = document.getElementById("viewer-icon")
+    const innerText = document.querySelectorAll('.nav-inner-text')
+    setNavOpen(!navOpen)
 
-    if (navOpen) {
-      navMain.style.width = '50px';
-      arrow.style.rotate = '360deg';
+    if(navOpen){
+      navMain.style.width = '50px'
+      arrow.style.rotate = '360deg'
       innerText.forEach(el => {
-        el.style.display = "none";
-      });
-    } else {
-      navMain.style.width = '250px';
-      arrow.style.rotate = '180deg';
+        el.style.display = "none"
+      })
+
+    }else{
+      navMain.style.width = '250px'
+      arrow.style.rotate = '180deg'
       innerText.forEach(el => {
-        el.style.display = "block";
-      });
+        el.style.display = "block"
+      })
     }
   }
-
   return (
-    <nav id="nav-main">
-      <div className="profile">
-        <ProfileButton />
-      </div>
-      <div className="big-btn-container">
-        <NavLink className='nav-btn-primary green' to="/">
-          <FontAwesomeIcon icon={faFileLines} />
-          <span className="nav-inner-text">Note</span>
-        </NavLink>
-        <NavLink className='nav-btn-primary purple' to="/">
-          <FontAwesomeIcon icon={faListCheck} />
-          <span className="nav-inner-text">Task</span>
-        </NavLink>
-      </div>
-      <ul id="nav-list">
-        <li>
-          <NavLink className='nav-btn-primary text-white' to="/">
-            <FontAwesomeIcon icon={faHouse} />
-            <span className="nav-inner-text">Home</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className='nav-btn-primary text-white' to="/">
-            <FontAwesomeIcon icon={faFileLines} />
-            <span className="nav-inner-text">Notes</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className='nav-btn-primary text-white' to="/">
-            <FontAwesomeIcon icon={faListCheck} />
-            <span className="nav-inner-text">Tasks</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className='nav-btn-primary text-white' to="/">
-            <FontAwesomeIcon icon={faBook} />
-            <span className="nav-inner-text">Notebooks</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className='nav-btn-primary text-white' to="/">
-            <FontAwesomeIcon icon={faTags} />
-            <span className="nav-inner-text">Tags</span>
-          </NavLink>
-        </li>
-      </ul>
+      <nav id="nav-main">
+        <ul id="nav-list">
+          <li className="nav-list-profile">
+            <ProfileButton />
+          </li>
+          <div className="big-btn-container">
+              <NavLink className='nav-btn-primary green' to="/">
+                <FontAwesomeIcon icon={faFileLines} />
+                <span className="nav-inner-text">Note</span>
+              </NavLink>
+
+              <NavLink className='nav-btn-primary purple' to="/">
+                <FontAwesomeIcon icon={faListCheck} />
+                <span className="nav-inner-text">Task</span>
+              </NavLink>
+          </div>
+          <li>
+            <NavLink className='nav-btn-primary text-white' to="/"><FontAwesomeIcon icon={faHouse} /><span className="nav-inner-text">Home</span></NavLink>
+          </li>
+          <li>
+            <NavLink className='nav-btn-primary text-white' to="/"><FontAwesomeIcon icon={faFileLines} /><span className="nav-inner-text">Notes</span></NavLink>
+          </li>
+          <li>
+            <NavLink className='nav-btn-primary text-white' to="/"><FontAwesomeIcon icon={faListCheck} /><span className="nav-inner-text">Tasks</span></NavLink>
+          </li>
+          <li>
+            <NavLink className='nav-btn-primary text-white' to="/"><FontAwesomeIcon icon={faBook} /><span className="nav-inner-text">Notebooks</span></NavLink>
+          </li>
+          <li>
+            <NavLink className='nav-btn-primary text-white' to="/"><FontAwesomeIcon icon={faTags} /><span className="nav-inner-text">Tags</span></NavLink>
+          </li>
+          {/* Maybe add a trash feature */}
+        </ul>
       <button id="nav-close-btn" onClick={navViewChanger}>
         <FontAwesomeIcon id="viewer-icon" icon={faArrowRight} />
       </button>
-    </nav>
+      </nav>
   );
 }
 
