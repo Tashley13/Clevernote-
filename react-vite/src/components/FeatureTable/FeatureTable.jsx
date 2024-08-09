a7309e3cd2a01fe409ed3836c5f067030984b69b
frank <imbruglia100@gmail.com>
2024-08-08 20:55:30 -0400

    styling

--- react-vite/src/components/FeatureTable/FeatureTable.jsx
+++ react-vite/src/components/FeatureTable/FeatureTable.jsx
@@ -1,29 +1,42 @@
 import { useDispatch, useSelector } from 'react-redux'
 import './FeatureTable.css'
 import { thunkGetNotebooks } from '../../redux/notebooks'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 const FeatureTable = () => {
 
-    const {allNotebooks} = useSelector(state => state.notebook)
+    const { allNotebooks } = useSelector(state => state.notebooks)
 
     const dispatch = useDispatch()
-    console.log(allNotebooks)
     useEffect(() => {
+        dispatch(thunkGetNotebooks())
 
-        if(!allNotebooks){
-            dispatch(thunkGetNotebooks())
-        }
 
-    }, [dispatch, allNotebooks])
+    }, [dispatch])
 
     return (
         allNotebooks ?
             <div id='table-main-container'>
-                <div>
-                    <h1>Notebooks</h1>
-                    <h3>{Object.keys(allNotebooks).length} {Object.keys(allNotebooks).length === 1 ? 'notebook' : 'notebooks'}</h3>
-                </div>
+                <h1>Notebooks</h1>
+                <h3>{Object.keys(allNotebooks).length} {Object.keys(allNotebooks).length === 1 ? 'notebook' : 'notebooks'}</h3>
+
+                <table id='feature-table-container'>
+                    <tr className='feature-tr-properies'>
+                        <th>Name</th>
+                        <th>Note Count</th>
+                        <th>Created at</th>
+                    </tr>
+                    {Object.values(allNotebooks).map(el => {
+                    return (
+                        <tr className='feature-tr-data'>
+                            <th>{el.title}</th>
+                            <th>{3}</th>
+                            <th>{el.created_at}</th>
+                        </tr>
+                    )
+                })}
+
+                </table>
             </div>
             :
             <h1>Loading</h1>
