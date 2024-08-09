10b1f9b516c7935e6654edfaf59f4ca8fb61e8b5
harmony <harmony.rosenberg94@gmail.com>
2024-08-08 19:19:34 -0500

    component created

--- react-vite/src/components/TagCreate/TagCreate.jsx
+++ react-vite/src/components/TagCreate/TagCreate.jsx
@@ -1,15 +1,36 @@
-// import React from 'react';
+import React, {useState} from 'react';
+import {createTag} from '../../redux/tags';
+import { useDispatch } from "react-redux";
+import { useModal } from '../../context/Modal';
 
 const TagCreate = () => {
+  const { closeModal } = useModal();
+  const dispatch = useDispatch()
+  const [tagName, setTagName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const newTag = {
+      tagName
+    }
+    dispatch(createTag(newTag)).then(() => {closeModal()})
+  }
+
   return (
     <div>
       <h1>Create Tag</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="tag_name">Tag Name:</label>
-          <input type="text" id="tag_name" name="tag_name" />
+          <label> Tag Name </label>
+          <input
+          type="text"
+          value={tagName}
+          onChange={(e) => setTagName(e.target.value)}
+          required
+          />
         </div>
-        <button type="submit">Create</button>
+        <button type="submit">Create Tag</button>
       </form>
     </div>
   );
