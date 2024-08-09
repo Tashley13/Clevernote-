129a738502e1cfd28f5015038e95f0ca635c4c7a
Chase Kliment <stepintoliquidd@gmail.com>
2024-08-08 20:04:37 -0600

    working on adding the fetch tasks on the homepage, having errors

--- react-vite/src/components/HomePage/HomePage.jsx
+++ react-vite/src/components/HomePage/HomePage.jsx
@@ -1,9 +1,25 @@
-import './HomePage.css';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTasks } from '../../redux/tasks'; // Make sure this path is correct
 
 const HomePage = () => {
+  const dispatch = useDispatch();
+  const tasks = useSelector(state => state.tasks);
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+
   return (
     <div className="homepage-container">
-     
+      <h2>My Tasks</h2>
+      {/* Render tasks here */}
+      {tasks && tasks.map(task => (
+        <div key={task.id}>
+          <p>{task.title}</p>
+          <p>{task.due_date}</p>
+        </div>
+      ))}
     </div>
   );
 };
--- react-vite/src/components/TaskList/TaskList.jsx
+++ react-vite/src/components/TaskList/TaskList.jsx
@@ -1,16 +1,33 @@
-// import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTasks } from '../../redux/tasks';
+import './TaskList.css';
 
-const TagList = () => {
+const TaskList = () => {
+  const dispatch = useDispatch();
+  const tasks = useSelector(state => state.tasks); // Ensure 'tasks' is set correctly in the store
+
+  useEffect(() => {
+    dispatch(fetchTasks()); // Fetch tasks on component mount
+  }, [dispatch]);
+
   return (
-    <div>
-      <h1>Tags</h1>
+    <div className="task-list">
+      <h2>My Tasks</h2>
       <ul>
-        <li>Tag 1</li>
-        <li>Tag 2</li>
-        <li>Tag 3</li>
+        {tasks && tasks.length > 0 ? (
+          tasks.map((task) => (
+            <li key={task.id} className={`task ${task.status === 'completed' ? 'completed' : ''}`}>
+              <span>{task.title}</span>
+              {/* Additional task details can be added here */}
+            </li>
+          ))
+        ) : (
+          <p>No tasks available.</p>
+        )}
       </ul>
     </div>
   );
 };
 
-export default TagList;
+export default TaskList;
