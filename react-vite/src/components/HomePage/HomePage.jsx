0b68e3a86b74fcfd64de8b0bf29afa70bb86f030
Chase Kliment <stepintoliquidd@gmail.com>
2024-08-09 19:38:56 -0600

    added My Tasks to home page

--- react-vite/src/components/HomePage/HomePage.jsx
+++ react-vite/src/components/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchTasks } from '../../redux/tasks'; // Make sure this path is correct
+import { fetchTasks } from '../../redux/tasks';
 
 const HomePage = () => {
   const dispatch = useDispatch();
@@ -11,15 +11,22 @@ const HomePage = () => {
   }, [dispatch]);
 
   return (
-    <div className="homepage-container">
-      <h2>My Tasks</h2>
-      {/* Render tasks here */}
-      {tasks && tasks.map(task => (
-        <div key={task.id}>
-          <p>{task.title}</p>
-          <p>{task.due_date}</p>
+    <div id="split-layout">
+      <nav id="nav-main">
+        {/* Your existing navigation */}
+      </nav>
+      <div className="homepage-container">
+        <div className="tasks-sidebar">
+          <h2>My Tasks</h2>
+          {/* Render tasks here */}
+          {tasks && tasks.map(task => (
+            <div key={task.id} className="task">
+              <p>{task.title}</p>
+              <p>{task.due_date}</p>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 };
