0de6df1dac13fda45a45715ebf961dfd96ac9549
harmony <harmony.rosenberg94@gmail.com>
2024-08-10 21:15:44 -0500

    started tag component, need to finish tagList component before i continue

--- react-vite/src/components/TagEdit/TagEdit.jsx
+++ react-vite/src/components/TagEdit/TagEdit.jsx
@@ -1,6 +1,27 @@
-// import React from 'react';
+import {thunkEditTag} from '../../redux/tags';
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, useNavigate } from 'react-router-dom';
 
 const TagEdit = () => {
+  const dispatch = useDispatch()
+	const navigate = useNavigate();
+  const [tagName, setTagName] = useState('')
+  const {tagId} = useParams()
+  const tag = useSelector(state => state.tags)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const editTag = {
+      tagName: tagName || tag.tagName
+    }
+
+    const updatedTag = await dispatch(thunkEditTag(editTag))
+  }
+
+  //use effect will go here once i make taglist component
+
   return (
     <div>
       <h1>Edit Tag</h1>
