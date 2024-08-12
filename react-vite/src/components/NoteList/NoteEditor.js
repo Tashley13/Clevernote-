13bcd6622467c3f401fdbd2a4e84147c2252d96a
Taylor <taylorashley1304@gmail.com>
2024-08-12 18:41:28 -0400

    incorporating React Quill with snow theme as Rich Text Editor

---
+++ react-vite/src/components/NoteList/NoteEditor.js
@@ -0,0 +1,49 @@
+import Quill from "quill";
+import React, { forwardRef, useEffect, useLayoutEffect, useRef } from "react";
+
+//create Editor to import into NoteList component
+const NoteEditor = forwardRef(
+    ({defaultValue, onTextChange, onSelectionChange }, ref ) => {
+        const containerRef = useRef(null);//create a container DOM element reference
+        const defaultValueRef = useRef(defaultValue); //store initial default value of note
+        const onTextChangeRef = useRef(onTextChange); //track text changes
+        const onSelectionChangeRef = useRef(onSelectionChange); //track toolbar selection
+
+        useLayoutEffect(() => { //useLayoutEffect fires nefre the browser repaints the screen
+            onTextChangeRef.current = onTextChange; //watch for text changes
+            onSelectionChangeRef.current = onSelectionChange //watch for selection changes
+        });
+
+        useEffect(() => {
+            const container = containerRef.current;
+            const editorContainer = container.appendChild(
+
+            container.ownerDocument.createElement('div'),
+            );
+            const quill = new Quill(editorContainer, {
+                theme: 'snow'
+            });
+
+            ref.current=quill;
+
+            if (defaultValueRef.current) {
+                quill.setContents(defaultValueRef.current);
+            }
+
+            quill.on(Quill.events.SELECTION_CHANGE,
+                (...args) => {
+                    onSelectionChangeRef.current?.(...args);
+                }
+            );
+
+            return () => {
+                ref.current = null;
+                container.innerHTML = '';
+            };
+        }, [ref]); //listen for ref
+    }
+)
+
+Editor.displayName = 'Editor'
+
+export default NoteEditor;
--- react-vite/src/components/NoteList/NoteList.jsx
+++ react-vite/src/components/NoteList/NoteList.jsx
@@ -3,60 +3,39 @@ import { useDispatch, useSelector } from "react-redux";
 import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from "react-router-dom";
 import * as noteActions from "../../redux/note";
-// import Editor from 'quill/core/editor';
+import NoteEditor from "../NoteList";
 // import React from 'react';
 //to create a note
 
-// const quillDelta = Quill.import('delta')
+const quillDelta = Quill.import('delta')
 
 
 const NoteList = () => {
 
   const dispatch = useDispatch();
   const { noteId, userId } = useParams()
-  const [lastChange, setLastChange] = useState()
+  const [setLastChange] = useState()
   // console.log('USERID: ', userId)
 
   //use a ref to access the quill instance directly
   const quillRef = useRef();
 
-
-
-  useEffect(() => {
-    const toolbarOptions = [
-      ['bold', 'italic', 'underline', 'strike'],
-
-      [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-      [{ 'header': [1, 2, 3, false] }]
-    ]
-
-    if (quillRef) {
-
-      const quill = new Quill(quillRef.current, {
-        theme: 'snow',
-        modules: {
-          toolbar: toolbarOptions
-        },
-        placeholder: 'Insert thoughts here...'
-      });
-      quill.on('text-change', () => {
-        setLastChange(quill.getContents());
-      })
-    }
-  }, [quillRef])
-
-  //update title
-  //update body
+  // //update title
+  // //update body
 
   return (
     <div>
       <div className="title">
       </div>
       <div className='editor-container'>
-        <div ref={quillRef}></div>
+        <NoteEditor
+        ref={quillRef}
+        defaultValue={new quillDelta()
+          .insert('Insert thoughts here...')
+        }
+        onTextChange={setLastChange}
+        />
       </div>
-
     </div>
   );
 };
