532c56a097bfe1aef11b6f5f24c036371a9d506c
frank <imbruglia100@gmail.com>
2024-08-13 20:35:20 -0400

    render dix

--- migrations/versions/83c61c29343a_.py
+++ migrations/versions/83c61c29343a_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 93e33911e147
+Revision ID: 83c61c29343a
 Revises: 
-Create Date: 2024-08-13 15:14:11.667665
+Create Date: 2024-08-13 20:09:01.231060
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '93e33911e147'
+revision = '83c61c29343a'
 down_revision = None
 branch_labels = None
 depends_on = None
