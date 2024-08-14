7ab6e4ddef283bf067155ec4a8b7e73e4c4ed8cc
frank <imbruglia100@gmail.com>
2024-08-14 14:41:46 -0400

    reset

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,6 +3,5 @@ from .user import User
 from .task import Task  # Add this line to import the Task model
 from .db import environment, SCHEMA
 from .notebooks import Notebook
-from .note import Note, note_tag
+from .note import Note
 from .tags import Tag
-# from .note_tag import note_tag
