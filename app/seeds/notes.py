bdd8b1cfc5eb55aed02f3244c9da0cf283262caa
frank <imbruglia100@gmail.com>
2024-12-21 10:29:20 -0500

    new notes seeders

--- app/seeds/notes.py
+++ app/seeds/notes.py
@@ -3,17 +3,17 @@ from sqlalchemy.sql import text
 
 def seed_notes():
  # Update Jane's notes to belong to userId=1
-    janes_notes = Note(title='Jane\' Notes', content='Jane content', notebookId=1, userId=1)
+    janes_notes = Note(title='Jane\' Notes', content='{"ops":[{"insert":"New and Improved."},{"attributes":{"header":2},"insert":"\n"},{"insert":"\nThis is the new note taking software. \n\n"},{"attributes":{"bold":true},"insert":"There can be bold!"},{"attributes":{"italic":true},"insert":" "},{"insert":"\n\n"},{"attributes":{"italic":true},"insert":"Also Italics."},{"insert":"\n\n"},{"attributes":{"italic":true},"insert":"And Best of ALL."},{"insert":"\n\n"},{"attributes":{"underline":true,"link":"https://m.media-amazon.com/images/I/614wvy1ldXL.__AC_SX300_SY300_QL70_FMwebp_.jpg"},"insert":"underline"},{"insert":".\n"}]}', notebookId=1, userId=1)
 
-    pauls_notes = Note(title='Paul\'s Notes', content='Paul content', notebookId=2, userId=2)
+    pauls_notes = Note(title='Paul\'s Notes', content='{"ops":[{"insert":"New and Improved."},{"attributes":{"header":2},"insert":"\n"},{"insert":"\nThis is the new note taking software. \n\n"},{"attributes":{"bold":true},"insert":"There can be bold!"},{"attributes":{"italic":true},"insert":" "},{"insert":"\n\n"},{"attributes":{"italic":true},"insert":"Also Italics."},{"insert":"\n\n"},{"attributes":{"italic":true},"insert":"And Best of ALL."},{"insert":"\n\n"},{"attributes":{"underline":true,"link":"https://m.media-amazon.com/images/I/614wvy1ldXL.__AC_SX300_SY300_QL70_FMwebp_.jpg"},"insert":"underline"},{"insert":".\n"}]}', notebookId=2, userId=2)
 
-    bobbys_notes = Note(title='Bobby\'s Notes', content='Bobby content', notebookId=3, userId=3)
+    bobbys_notes = Note(title='Bobby\'s Notes', content='{"ops":[{"insert":"New and Improved."},{"attributes":{"header":2},"insert":"\n"},{"insert":"\nThis is the new note taking software. \n\n"},{"attributes":{"bold":true},"insert":"There can be bold!"},{"attributes":{"italic":true},"insert":" "},{"insert":"\n\n"},{"attributes":{"italic":true},"insert":"Also Italics."},{"insert":"\n\n"},{"attributes":{"italic":true},"insert":"And Best of ALL."},{"insert":"\n\n"},{"attributes":{"underline":true,"link":"https://m.media-amazon.com/images/I/614wvy1ldXL.__AC_SX300_SY300_QL70_FMwebp_.jpg"},"insert":"underline"},{"insert":".\n"}]}', notebookId=3, userId=3)
 
-    bobbies_notes2 = Note(title='Bobbie\'s Notes2', content='Bobbie content2', notebookId=1, userId=3)
+    bobbies_notes2 = Note(title='Bobbie\'s Notes2', content='{"ops":[{"insert":"New and Improved."},{"attributes":{"header":2},"insert":"\n"},{"insert":"\nThis is the new note taking software. \n\n"},{"attributes":{"bold":true},"insert":"There can be bold!"},{"attributes":{"italic":true},"insert":" "},{"insert":"\n\n"},{"attributes":{"italic":true},"insert":"Also Italics."},{"insert":"\n\n"},{"attributes":{"italic":true},"insert":"And Best of ALL."},{"insert":"\n\n"},{"attributes":{"underline":true,"link":"https://m.media-amazon.com/images/I/614wvy1ldXL.__AC_SX300_SY300_QL70_FMwebp_.jpg"},"insert":"underline"},{"insert":".\n"}]}', notebookId=1, userId=3)
 
-    bobbies_notes3 = Note(title='Bobbie\'s Notes3', content='Bobbie content3', notebookId=1, userId=3)
+    bobbies_notes3 = Note(title='Bobbie\'s Notes3', content='{"ops":[{"insert":"New and Improved."},{"attributes":{"header":2},"insert":"\n"},{"insert":"\nThis is the new note taking software. \n\n"},{"attributes":{"bold":true},"insert":"There can be bold!"},{"attributes":{"italic":true},"insert":" "},{"insert":"\n\n"},{"attributes":{"italic":true},"insert":"Also Italics."},{"insert":"\n\n"},{"attributes":{"italic":true},"insert":"And Best of ALL."},{"insert":"\n\n"},{"attributes":{"underline":true,"link":"https://m.media-amazon.com/images/I/614wvy1ldXL.__AC_SX300_SY300_QL70_FMwebp_.jpg"},"insert":"underline"},{"insert":".\n"}]}', notebookId=1, userId=3)
 
-    demo_notes = Note(title='Demo\'s Notes', content='Demo content', notebookId=4, userId=4)
+    demo_notes = Note(title='Demo\'s Notes', content='{"ops":[{"insert":"New and Improved."},{"attributes":{"header":2},"insert":"\n"},{"insert":"\nThis is the new note taking software. \n\n"},{"attributes":{"bold":true},"insert":"There can be bold!"},{"attributes":{"italic":true},"insert":" "},{"insert":"\n\n"},{"attributes":{"italic":true},"insert":"Also Italics."},{"insert":"\n\n"},{"attributes":{"italic":true},"insert":"And Best of ALL."},{"insert":"\n\n"},{"attributes":{"underline":true,"link":"https://m.media-amazon.com/images/I/614wvy1ldXL.__AC_SX300_SY300_QL70_FMwebp_.jpg"},"insert":"underline"},{"insert":".\n"}]}', notebookId=4, userId=4)
 
     tag1=Tag(tag_name='Test Tag 1', user_id=1)
     tag2=Tag(tag_name='Test Tag 2', user_id=2)
